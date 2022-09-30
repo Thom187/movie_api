e88546f377e7df74d12e4a678dba599146b60982
Thom187 <tom.hoppe@gmx.de>
2022-09-30 16:12:12 +0200

    Reorganised code

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ const app = express();
 
 app.use(morgan('common'));
 
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// Automatically route all requests for static files to folder /public
+app.use(express.static('public'));
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', {root: __dirname});
+})
+
+// Add a error handler and log information (via the error property err.stack)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something does not work!');
+});
+
+// listen for any requests
+app.listen(8080, () => {
+  console.log('myFlix is listening on port 8080.');
+});
+
+
 // Create an array of movies to load it as JSON
 let topMovies = [
   {
@@ -49,32 +77,3 @@ let topMovies = [
     director: 'Director 10'
   },
 ];
-
-
-
-// GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to myFlix!');
-});
-
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
-
-// Automatically route all requests for static files to folder /public
-app.use(express.static('public'));
-
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', {root: __dirname});
-})
-
-// Add a error handler and log information (via the error property err.stack)
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something does not work!');
-});
-
-// listen for any requests
-app.listen(8080, () => {
-  console.log('myFlix is listening on port 8080.');
-});
