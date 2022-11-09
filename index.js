846dfbf2adc40a545c3f386f17ef9f22ac55c3bc
Thom187 <tom.hoppe@gmx.de>
2022-11-09 11:11:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 // Read/ Return a list of all movies
 app.get(
   "/movies",
-  // passport.authenticate("jwt", { session: false }),
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
       const movies = await Movies.find();
